f5b5e490606246ab056109fb267207ce903a5b53
Michael Federczuk <federczuk.michael@protonmail.com>
2023-01-06 19:18:21 +0100

    Remove all source files

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-/*
- * SPDX-License-Identifier: CC0-1.0
- */
-
-export * from "./playlist";
-export * from "./track";
--- src/mutable.ts
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: CC0-1.0
-
-export type Mutable<T> = {
-	-readonly [P in keyof T]: T[P];
-};
-
-export type DeepMutable<T> = {
-	-readonly [P in keyof T]: DeepMutable<T[P]>;
-};
